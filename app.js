ed34b7223b72858e934ac0dc34fdc869eac6d6fb
Dan Moneypenny <catullus1973@gmail.com>
2022-02-26 01:05:15 -0800

    add static files

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+app.use(express.static('public'))
+
 app.set('view engine', 'ejs');
 
 app.locals.caption = "this is a caption"
